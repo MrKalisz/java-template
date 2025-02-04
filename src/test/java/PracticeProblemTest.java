d1cbaba50c2a5d21a61ac2864ed9a7d124abf69e
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-02-04 08:49:50 -0500

    Update and rename Review1Test.java to PracticeProblemTest.java

---
+++ src/test/java/PracticeProblemTest.java
@@ -0,0 +1,35 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+import java.io.*;
+
+public class PracticeProblemTest {
+
+   @Test
+   public void testOutput()
+   {
+     PrintStream originalOut = System.out;
+     ByteArrayOutputStream bos = new ByteArrayOutputStream();
+     System.setOut(new PrintStream(bos));
+
+     // action
+     PracticeProblem.q1();
+
+     // assertion
+     assertEquals("There once was a man from St. Ives.\n", bos.toString());
+
+     // undo the binding in System
+     System.setOut(originalOut);
+   }
+
+   @Test
+   public void testQ2()
+   {
+     
+   }
+
+   @Test
+   public void testQ3()
+   {
+     
+   }
+}
