1230df6ca205aa7e01b861ba5678568546b2f858
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2026-03-11 10:43:50 -0400

    Update PracticeProblemTest.java

--- src/test/java/PracticeProblemTest.java
+++ src/test/java/PracticeProblemTest.java
@@ -46,4 +46,19 @@ public class PracticeProblemTest {
    {
      
    }
+
+   @Test
+   @DisplayName("")
+   void isEvenTest1() {
+      Class<?> testClass = PracticeProblem.class;
+      try {
+         Class[] cArg = { int.class };
+         Method method = testClass.getDeclaredMethod("isEven", cArg);
+         assertEquals(true, (boolean) method.invoke(null, 4));
+      } catch (NoSuchMethodException e) {
+         fail("Method does not exist");
+      } catch (Exception e) {
+         fail("Something weird happened: " + e);
+      }
+   }
 }
